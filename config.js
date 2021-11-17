a95e179dcea4e5e5a4457e57e76a50cf935b7eb4
Quangitcoder1123 <94438536+Quangitcoder1123@users.noreply.github.com>
2021-11-17 10:19:40 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'CHỊ IU À!!',
+    introDesc: `Trái đất vốn 7 tỷ người mà sao bây giờ vẫn chưa có bồ 😙😙`,
+    btnIntro: 'Khai mau',
+    title: 'Phải chăng CỊ IU đã yêu A HÀNG XÓM ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng chị iu đã có người yêu 😘😘 ',
+    btnYes: 'CÓ RỒI NHÉ THẰNG KIA<33',
+    btnNo: 'CÓ CÁI NỊT ĐANG Ế TẬN CỔ NÈ :3',
+    question:'Trên thế giới hơn 7 tỉ người mà lại iu a hàng xóm v: <3',
+    btnReply: 'Gửi cho anh hàng xóm nhé <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh Khoa biết mà 🥰. Yêu CHỊ 😘😘',
+    messDesc: 'CÓ NY RỒI MÀ K BÁO VỚI BA VS MẸ',
+    btnAccept: 'ĐƯỢC LẮM',
+    messLink: 'https://www.facebook.com/profile.php?id=100022481742599'
+}
